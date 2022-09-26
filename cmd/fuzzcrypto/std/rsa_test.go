3d206f2e8e57806290a6d11469aaddec2b5c6fc8
Davis Goodin <dagood@microsoft.com>
2022-09-26 09:54:39 -0700

    Check error message, not key length itself

--- cmd/fuzzcrypto/std/rsa_test.go
+++ cmd/fuzzcrypto/std/rsa_test.go
@@ -120,17 +120,14 @@ func FuzzRSASignPSS(f *testing.F) {
 		if key.Validate() != nil {
 			return
 		}
-		// Check if key size is too small for PSS signature, based on crypto/rsa/pss.go
-		hLen := crypto.SHA256.Size()
-		sLen := hLen // Same as hash size thanks to SaltLength option.
-		emBits := key.N.BitLen() - 1
-		emLen := (emBits + 7) / 8
-		if emLen < hLen+sLen+2 {
-			return // "crypto/rsa: key size too small for PSS signature"
-		}
 		hashed := sha256.Sum256(msg)
 		sig, err := rsa.SignPSS(rand.Reader, key, crypto.SHA256, hashed[:], &opts)
 		if err != nil {
+			switch err.Error() {
+			// Key size being too small is an invalid input, not a problem.
+			case "crypto/rsa: key size too small for PSS signature", "crypto/rsa: invalid key size":
+				return
+			}
 			t.Fatal(err)
 		}
 		err = rsa.VerifyPSS(&key.PublicKey, crypto.SHA256, hashed[:], sig, &opts)
