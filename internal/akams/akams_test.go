aaa59affa4f735f06321b62790658c5eafa3c819
qmuntal <qmuntaldiaz@microsoft.com>
2024-03-04 18:29:38 +0100

    add akams tests

---
+++ internal/akams/akams_test.go
@@ -0,0 +1,88 @@
+package akams_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+
+	"github.com/microsoft/go-infra/internal/akams"
+)
+
+func TestNewClient(t *testing.T) {
+	_, err := akams.NewClient("tenant", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = akams.NewClient("tenant", &http.Client{})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestNewClientCustom(t *testing.T) {
+	_, err := akams.NewClientCustom("https://example.com", akams.HostO365COM, "tenant", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = akams.NewClientCustom("https://example.com", akams.HostO365COM, "tenant", &http.Client{})
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = akams.NewClientCustom("://example.com", akams.HostO365COM, "tenant", nil)
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+}
+
+func TestCreateBulk(t *testing.T) {
+	links := []akams.Link{
+		{ShortURL: "short", TargetURL: "target"},
+		{ShortURL: "short2", TargetURL: "target2"},
+	}
+	tentant := "tenant"
+	host := akams.HostO365COM
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			t.Errorf("expected POST, got %s", r.Method)
+		}
+		if want := fmt.Sprintf("/aka/%s/%s/bulk", host, tentant); r.URL.Path != want {
+			t.Errorf("expected path %s, got %s", want, r.URL.Path)
+		}
+		var got []akams.Link
+		if err := json.NewDecoder(r.Body).Decode(&got); err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(got, links) {
+			t.Errorf("expected %+v, got %+v", links, got)
+		}
+	}))
+	client, err := akams.NewClientCustom(srv.URL, host, tentant, srv.Client())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := client.CreateBulk(context.Background(), links); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestCreateBulkFail(t *testing.T) {
+	srv := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("fake error"))
+	}))
+	client, err := akams.NewClientCustom(srv.URL, akams.HostO365COM, "tenant", srv.Client())
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = client.CreateBulk(context.Background(), []akams.Link{})
+	if err == nil {
+		t.Fatal("expected error, got nil")
+	}
+	if want, got := "request failed: 500\nfake error", err.Error(); want != got {
+		t.Errorf("expected: %q, got: %q", want, got)
+	}
+}
