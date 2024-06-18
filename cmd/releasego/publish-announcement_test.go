776abb4c1aa4d6129a4b4cd867018aec491ad2be
mertakman <mertakman2012@gmail.com>
2024-06-18 08:01:35 +0100

    fix: release version generator test

--- cmd/releasego/publish-announcement_test.go
+++ cmd/releasego/publish-announcement_test.go
@@ -37,7 +37,7 @@ func TestGenerateSlug(t *testing.T) {
 }
 
 func TestGoReleaseVersionLink(t *testing.T) {
-	releaseID := "1.22.3"
+	releaseID := "go1.22.3"
 	expected := "https://go.dev/doc/devel/release#go1.22.3"
 
 	result := createGoReleaseLinkFromVersion(releaseID)
