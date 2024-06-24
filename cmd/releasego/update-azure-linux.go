1a9f7933256b101b8c88a9847cf90023800f4ebe
mertakman <mertakman2012@gmail.com>
2024-06-24 03:46:59 +0100

    fix:add a regexp for updating the signatures file content

--- cmd/releasego/update-azure-linux.go
+++ cmd/releasego/update-azure-linux.go
@@ -3,7 +3,12 @@
 
 package main
 
-import "github.com/microsoft/go-infra/subcmd"
+import (
+	"flag"
+	"regexp"
+
+	"github.com/microsoft/go-infra/subcmd"
+)
 
 func init() {
 	subcommands = append(subcommands, subcmd.Option{
@@ -15,5 +20,45 @@ func init() {
 }
 
 func updateAzureLinux(p subcmd.ParseFunc) error {
+	var buildAssetJSON string
+
+	flag.StringVar(&buildAssetJSON, "build-asset-json", "", "[Required] The path of a build asset JSON file describing the Go build to update to.")
+
+	if err := p(); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+const (
+	golangSignaturesFilepath = "SPECS/golang/golang.signatures.json"
+	golangSpecFilepath       = "SPECS/golang/golang.spec"
+	cgManifestJSON           = "cgmanifest.json"
+)
+
+func updateSignaturesFile(signatureFileContent []byte, msGoFilename, msGoRevision, version string) []byte {
+	content := string(signatureFileContent)
+
+	// Define the regex patterns
+	msGoFilenamePattern := regexp.MustCompile(`(%global ms_go_filename\s+)\S+`)
+	msGoRevisionPattern := regexp.MustCompile(`(%global ms_go_revision\s+)\d+`)
+	versionPattern := regexp.MustCompile(`(Version:\s+)\d+\.\d+\.\d+`)
+
+	// Replace the matched patterns with the new values
+	content = msGoFilenamePattern.ReplaceAllString(content, `${1}`+msGoFilename)
+	content = msGoRevisionPattern.ReplaceAllString(content, `${1}`+msGoRevision)
+	content = versionPattern.ReplaceAllString(content, `${1}`+version)
+
+	return []byte(content)
+}
+
+func updateSpecFile() ([]byte, error) {
+
+	return nil, nil
+}
+
+func updateCGManifest() ([]byte, error) {
+
+	return nil, nil
 }
