ecd9682fc00d9cbbdaac5b8e6753852a35011e44
mertakman <mertakman2012@gmail.com>
2024-08-19 01:12:03 +0100

    fix:fix name of the command

--- cmd/releasego/azdo-pipeline-trigger.go
+++ cmd/releasego/azdo-pipeline-trigger.go
@@ -4,7 +4,7 @@ import "github.com/microsoft/go-infra/subcmd"
 
 func init() {
 	subcommands = append(subcommands, subcmd.Option{
-		Name:    "update-azure-linux",
+		Name:    "azdo-pipeline-trigger",
 		Summary: "Trigger the Azure DevOps pipeline for Azure Linux.",
 		Description: `
 This command triggers the Azure DevOps pipeline for Azure Linux.
