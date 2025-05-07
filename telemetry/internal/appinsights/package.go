6e6aa10372a501d820091cc15927102edeeb3c6f
qmuntal <qmuntaldiaz@microsoft.com>
2025-05-07 12:22:18 +0200

    add copyright headers

--- telemetry/internal/appinsights/package.go
+++ telemetry/internal/appinsights/package.go
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 // Package appinsights provides an interface to submit telemetry to Application Insights,
 // a component of Azure Monitor. This package calls the Classic API.
 // See https://learn.microsoft.com/en-us/azure/azure-monitor/app/api-custom-events-metrics
