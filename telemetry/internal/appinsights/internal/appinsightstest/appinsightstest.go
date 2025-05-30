0975b5f7eb83692201525d8fff777fd49cb74962
Davis Goodin <dagood@microsoft.com>
2025-05-30 10:16:07 -0700

    Fix build ./... without synctest: add experiment tag

--- telemetry/internal/appinsights/internal/appinsightstest/appinsightstest.go
+++ telemetry/internal/appinsights/internal/appinsightstest/appinsightstest.go
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+//go:build goexperiment.synctest
+
 // This package contains test utilities for the Application Insights SDK.
 package appinsightstest
 
