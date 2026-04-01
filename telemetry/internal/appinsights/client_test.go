3a76f7f79ba07921734c19a84240b2232a70c5c7
Davis Goodin <dagood@users.noreply.github.com>
2026-04-01 10:38:58 -0700

    Skip flaky App Insights tests (#456)
    * Skip flaky App Insights tests
    
    * Add additional flaky skip

--- telemetry/internal/appinsights/client_test.go
+++ telemetry/internal/appinsights/client_test.go
@@ -117,6 +117,7 @@ func TestClientBatchMultiple(t *testing.T) {
 }
 
 func TestClientInterval(t *testing.T) {
+	t.Skip("flaky: https://github.com/microsoft/go-lab/issues/253")
 	plan := testPlan{
 		maxBatchInterval: 1 * time.Second,
 		actions: []appinsightstest.Action{
@@ -134,6 +135,7 @@ func TestClientInterval(t *testing.T) {
 }
 
 func TestClientIntervalMultiple(t *testing.T) {
+	t.Skip("flaky: https://github.com/microsoft/go-lab/issues/253")
 	plan := testPlan{
 		maxBatchInterval: 1 * time.Second,
 		actions: []appinsightstest.Action{
@@ -185,6 +187,7 @@ func TestClientFlushMultiple(t *testing.T) {
 }
 
 func TestClientBatchIntervalFlush(t *testing.T) {
+	t.Skip("flaky: https://github.com/microsoft/go-lab/issues/253")
 	plan := testPlan{
 		maxBatchSize:     2,
 		maxBatchInterval: 1 * time.Second,
