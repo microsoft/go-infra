3beef1cf79a0d018de84890f8228c51bdf89d389
michelle-clayton-work <mclayton+github@microsoft.com>
2026-08-29 10:23:03 -0700

    releaseagent: remove unused full release workflow

--- cmd/releaseagent/main.go
+++ cmd/releaseagent/main.go
@@ -4,13 +4,8 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
-	"slices"
 
-	"github.com/microsoft/go-infra/cmd/releaseagent/internal/fullrelease"
-	"github.com/microsoft/go-infra/goversion"
 	"github.com/microsoft/go-infra/subcmd"
 )
 
@@ -25,44 +20,4 @@ func main() {
 	if err := subcmd.Run("releaseagent", description, subcommands); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func BindInputFlags() *fullrelease.Input {
-	var i fullrelease.Input
-
-	flag.Func(
-		"version",
-		"A version to release. Pass the flag multiple times to release multiple versions",
-		func(s string) error {
-			v := goversion.New(s) // Ensure it's a valid version and normalize.
-
-			// Check for obvious mistakes.
-			if v.Major != "1" {
-				return fmt.Errorf("major version must be 1, got %q in %q", v.Major, v)
-			}
-			if slices.Contains(i.Versions, s) {
-				return fmt.Errorf("duplicate version passed: %q", s)
-			}
-
-			i.Versions = append(i.Versions, v.Full())
-			return nil
-		})
-
-	flag.BoolVar(&i.Security, "security", false, "This release contains security fixes. Changes announcement text")
-	flag.StringVar(&i.RunnerGitHubUser, "runner", "", "GitHub username of the dev in charge of this release")
-
-	flag.StringVar(
-		&i.ReleaseConfigVariableGroup,
-		"release-config-variable-group", "",
-		"AzDO variable group containing the release configuration. Mostly secrets")
-
-	return &i
-}
-
-func BindSecretFlags() *fullrelease.Secret {
-	var s fullrelease.Secret
-	flag.StringVar(&s.GitHubPAT, "github-pat", "", "GitHub PAT with write access to microsoft/go")
-	flag.StringVar(&s.GitHubReviewerPAT, "github-reviewer-pat", "", "GitHub PAT for microsoft/go approver account")
-	flag.StringVar(&s.AzDOPAT, "azdo-pat", "", "Azure DevOps Personal Access Token (typically System.AccessToken)")
-	return &s
 }
